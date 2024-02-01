e38ae31edfc4eacab8db9eb35cccde2996d26a6b
sneha <snehasatheesh176@gmail.com>
2024-02-01 22:44:14 +0530

    changes in model

--- mypro/themes/models.py
+++ mypro/themes/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Sitesetting(models.Model):
-    banner=models.ImageField(upload_to='media/site')
-    caption=models.TextField()
+      banner= models.ImageField(upload_to='media',null=True,blank=True)
+      caption=models.TextField()
